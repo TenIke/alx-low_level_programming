8e626eadb55a152e22998075bac055bd174f3d8f
TenIke <anthonydzani@gmail.com>
2023-03-16 18:00:37 -0700

    print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void)
+
 {
 	char acharacter;
 	int ainteger;
@@ -10,8 +11,8 @@ int main(void)
 
 	printf("Size of a cha: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of a int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n" sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n" sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n" sizeof(afloat));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }
