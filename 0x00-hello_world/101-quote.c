031518dee3345c2ca8bd121c753940b119cd8eb3
TenIke <anthonydzani@gmail.com>
2023-03-17 07:01:14 -0700

    quote with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
 * main - entry point
 * Description: 'writing a print code without using peintf or puts'
-*Return: value of 1
+*Return: value of (1)
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 		return (1);
 }
 
