bf4f34b702b78c5b1d01448b0966ab1e4c9ddfa6
TenIke <anthonydzani@gmail.com>
2023-03-17 06:58:52 -0700

    quote with write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - entry point
+* Description: 'writing a print code without using peintf or puts'
+*Return: value of 1
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
+
