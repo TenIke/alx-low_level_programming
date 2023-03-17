ce3845633e010184681de560443b92118a0bfaee
TenIke <anthonydzani@gmail.com>
2023-03-16 18:03:26 -0700

    print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	float afloat;
 
 	printf("Size of a cha: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of a int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
