a587f3ff5e569af70c5bb8f364b68151191d90de
TenIke <anthonydzani@gmail.com>
2023-03-20 16:12:20 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*Main - Start entry
+*
+*Description:
+*
+*Return: Always (0)
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	printf("%i", digit);
+	putchar('\n');
+
+	return (0);
+}
