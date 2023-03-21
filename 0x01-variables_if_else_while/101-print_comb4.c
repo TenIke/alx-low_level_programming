f84db89d268b2fd34d1fc0c6b091a7d59a8df6fe
TenIke <anthonydzani@gmail.com>
2023-03-20 17:37:30 -0700

    print combination101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *Main - Start entry
+ *
+ *Description
+ *
+ *Return: Alwarys 0 (Success)
+ */
+int main(void)
+{
+	int hund;
+	int tens;
+	int ones;
+
+	for (hund = 0; hund <= 9; hund++)
+	{
+		for (tens = hund + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hund + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hund < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
