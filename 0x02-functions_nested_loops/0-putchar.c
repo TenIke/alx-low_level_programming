a9b6d2eb642b2c747b2111887d78fb4216e1a2d8
TenIke <anthonydzani@gmail.com>
2023-03-21 07:05:57 -0700

    using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main - Start entries
+ *
+ * Description: write a program using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char p[6] = "TenIke";
+
+	for (i = 0; i < 6; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return(0);
+}
