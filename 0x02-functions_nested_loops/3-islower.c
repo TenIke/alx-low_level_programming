4d51df315177ac4ef7d2b183cb5133480b55a4a7
TenIke <anthonydzani@gmail.com>
2023-03-21 14:56:19 -0700

    printing in lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_islower - function name
+ *
+ *Description: checks if it is caselower
+ *
+ *Characters in ASCII code
+ *
+ *Return: 1 for lowercase. 0 for the rest
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
