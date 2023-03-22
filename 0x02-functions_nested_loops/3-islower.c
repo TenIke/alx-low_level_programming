24bcc08cfa703c99db78b28ce5845de2b1d9d80d
TenIke <anthonydzani@gmail.com>
2023-03-22 14:46:59 -0700

    isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_islower - function name
- *
- *Description: checks if it is caselower
- *
- *Characters in ASCII code
- *
- *Return: 1 for lowercase. 0 for the rest
- *
- */
+*_islower - function name
+*
+*Description: checks if it is caselower
+*
+*Characters in ASCII code
+*
+*Return: 1 for lowercase. 0 for the rest
+*
+*/
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
