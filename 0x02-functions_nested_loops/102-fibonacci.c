3b64c70ded8cf9a8658b1fd54606eb8e1b21b0e2
TenIke <anthonydzani@gmail.com>
2023-03-23 07:49:09 -0700

    fibonacci 400000

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Prints first 50 numbers of the fibonacci sequence
+* 1. Declare variables
+* 2. Initialize variables
+* 3. Loop through the fibonacci sequence
+* 4. Print the fibonacci sequence
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i, l;
+	long n1, n2, next;
+
+	n1 = 1;
+	n2 = 2;
+	l = 50;
+
+	for (i = 0; i < l; i++)
+	{
+		if (i == 49)
+		{
+			printf("%lu", n1);
+		}
+		else
+		{
+			printf("%lu, ", n1);
+			next = n1 + n2;
+			n1 = n2;
+			n2 = next;
+		}
+	}
+	printf("\n");
+	return (0);
+}
