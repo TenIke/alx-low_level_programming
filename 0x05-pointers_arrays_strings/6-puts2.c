6daf621fb098349b9f6109470995d256dc3bb747
TenIke <anthonydzani@gmail.com>
2023-03-28 15:02:15 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*puts2 - print pair values.
+*@str: value to be evaluate.
+*Return: nothing.
+*/
+void puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
