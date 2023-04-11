6fcc4edf8dc4c8eca917fddcfff0de8f2be72f38
TenIke <anthonydzani@gmail.com>
2023-04-11 08:43:30 -0700

    Error

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	printf("%d\n", result);
+	return (0);
+}
