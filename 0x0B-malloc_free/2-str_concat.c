40d095f13c0b4033acb9a77fa0726ca349f0a025
TenIke <anthonydzani@gmail.com>
2023-04-12 07:17:08 -0700

    two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,32 +8,28 @@
  **/
 char *str_concat(char *s1, char *s2)
 {
-	char *strDup;
-	int a, b;
+	char *concat_str;
+	int index, concat_index = 0,  len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	a = b = 0;
-	while (s1[a] != '\0')
-		a++;
-	while (s2[b] != '\0')
-		b++;
-	strDup = malloc(sizeof(char) * (a + b + 1));
-	if (strDup == NULL)
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
-	a = b = 0;
-	while (s1[a] != '\0')
-	{
-		strDup[a] = s1[a];
-		a++;
-	}
-	while (s1[b] != '\0')
-	{
-		strDup[a] = s2[b];
-		a++, b++;
-	}
-	strDup[a] = '\0';
-	return (strDup);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
