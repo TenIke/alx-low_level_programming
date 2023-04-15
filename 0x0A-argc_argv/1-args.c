d06fa65e3645200b56eccffeb54eef414c4a2257
TenIke <anthonydzani@gmail.com>
2023-04-15 15:52:03 -0700

    number of argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,9 @@
  * @argv: array of pointer to arguement strings
  * Return: 0
  **/
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
+	argv = argv;
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
