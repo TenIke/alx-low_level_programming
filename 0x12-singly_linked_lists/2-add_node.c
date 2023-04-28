ae11aab37265105c6bc58a10af519cc70d772112
TenIke <anthonydzani@gmail.com>
2023-04-28 15:53:23 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
-#include "lists."
+#include "lists.h"
 /**
  * add_node - add new nodes to the list
  * @head: current place in the list
  * @str: string to add to the head
  * Return: pointer to current position in list
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
