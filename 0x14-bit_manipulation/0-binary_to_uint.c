8fcc6b644db562f4af972e141036fcb200f87ef1
TenIke <anthonydzani@gmail.com>
2023-05-06 05:51:37 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,42 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: pointing to a string
  *
  * Return: unsigned int with decimal value or zero if error.
  */
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 	unsigned int weight = 1;
-	char *x = b;
-
+	const char *x = b;
 	/* checking if the string is empty or not */
-
 	if (b == NULL)
 		return (0);
-
 	/* moving x to the end of the string */
-
-	while (*x != ‘\0’)
+	while (*x != '\0')
 	{
 		x++;
 	}
-
 	/* moving x one step back to be on the last character */
-
 	x--;
 	while (x >= b)
 	{
-	/* checking for valid character */
-
-		if (*x == ‘1’)
+		/* checking for valid character */
+		if (*x == '1')
 		/* if the input is 1, add to weight */
 		{
-			result += weight;
+		result += weight;
 		}
-		else if (*x != ‘0’)
+		else if (*x != '0')
 		/* checkig if the input is invalid, return 0 if invalid */
-		{
-			return (0);
+	{
+		return (0);
+		}
 		/* updating the weight value, and move pointer one char backwards */
 		weight *= 2;
 		x--;
 	}
-	return (result);
+		return (result);
 }
